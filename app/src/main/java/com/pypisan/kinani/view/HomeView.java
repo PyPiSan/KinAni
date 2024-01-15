235e26affc34c992d05916c3f4cf9c560fbaec2e
PyPiSan <sanjeev.mishra96@gmail.com>
2024-01-15 22:15:29 +0530

    More page created for trending and recommendation

--- app/src/main/java/com/pypisan/kinani/view/HomeView.java
+++ app/src/main/java/com/pypisan/kinani/view/HomeView.java
@@ -7,9 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
